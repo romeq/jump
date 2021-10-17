cdd801a146b9743c11ceb29e02349734ddd88942
toke8677 <toke8677@gmail.com>
2021-10-17 18:02:47 +0200

    Updated binary name in help menu

--- src/utils.c
+++ src/utils.c
@@ -11,18 +11,19 @@
 void
 usage()
 {
-    fprintf(stderr, "Use alias: \"./jump [alias]\". To use it with cd, use command substitution\n");
+    fprintf(stderr, "Use alias: \"./jmp [alias]\". To use it with cd, use command substitution\n");
     fprintf(stderr, "For full help menu, use the -h flag.\n");
 }
 
 void
 verbose_usage()
 {
-    printf("Use alias:\t ./jump [alias] \n");
-    printf("New alias:\t jump [alias] [full path] \n");
-    printf("Remove alias:\t jump -d [alias] \n");
-    printf("Reset database:\t jump -r\n\n");
+    printf("Use alias:\t ./jmp <alias> \n");
+    printf("New alias:\t jmp <alias> <full path> \n");
+    printf("Remove alias:\t jmp -d <alias> \n");
+    printf("Reset database:\t jmp -r\n\n");
     printf("Optional flags:\n");
+    printf("\t-h: Show help menu\n");
     printf("\t-d: Delete alias. Alias argument required.\n");
     printf("\t-r: Reset database. This flag must be used alone, and asks for confirmation before continuing.\n");
 }
